6d6e0c1f84722ff3ae697ed50321cd757eb20ca7
RafikFarhad <rafikfarhad@gmail.com>
2017-11-02 00:39:07 +0600

    without redux web app

---
+++ js/components/VideoList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import VideoItem from './VideoItem';
+
+class VideoList extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    onSelectClick = (id) => {
+        this.props.onSelectClick(id);
+    }
+
+    render() {
+        console.log(this.props.videos);
+        return (
+            <ul className="list-group">
+                {
+                    this.props.videos.map((video, i) => {
+                        return <VideoItem onSelectClick={this.onSelectClick.bind(this)}
+                        id={i} 
+                        key={video.etag}
+                        video={video} />;
+                    })
+                }
+            </ul>
+        );
+    }
+}
+
+
+export default VideoList;
+
+
+
